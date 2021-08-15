afa19c4eb9410baee8a0723ae0706e8f61d55210
ark10806 <ark10806@gmail.com>
2021-08-15 17:26:45 +0900

    hi

--- Touch_probe/panel.py
+++ Touch_probe/panel.py
@@ -186,6 +186,9 @@ class MyApp(QWidget):
 
     def handle_results(self):
         serial = self.serial.toPlainText()
+        print(f'self.DB.is_unique(serial): {self.DB.is_unique(serial)}')
+        print(f'len(self.Axises): {len(self.Axises)}')
+        print(f'int(self.n_Axis.toPlainText()): {int(self.n_Axis.toPlainText())}')
         if self.DB.is_unique(serial) and len(self.Axises)==int(self.n_Axis.toPlainText()):
             # print(f'self.DB.is_unique(serial): {self.DB.is_unique(serial)}')
             # print(f'len(self.Axises): {len(self.Axises)}')
