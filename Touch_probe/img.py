f5199e8d7d2da3c60d3adddce0d7c696782868fc
ark10806 <ark10806@gmail.com>
2022-01-13 17:21:50 +0900

    rm

--- Touch_probe/img.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-import parameter as param
-
-img = Image.open(param.img_path)
-print(img.size[0], img.size[1])
-img = img.resize((int(img.size[0]/2), int(img.size[1]/2)))
-img.save(param.base + 'src/new_logo.png')
